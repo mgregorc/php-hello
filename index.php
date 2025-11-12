b62fa2180c84be74612555c5c1052b7f046cadc8
Marcin Gregorczyk <mgregorc@dell.mg>
2025-11-12 12:34:58 +0100

    v2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "Hello, World!<br><br>";
-#echo "Hostname: " . gethostname();
-#echo "<br><br>php versja:" . phpversion();
-#echo "<br>";
+echo "Hostname: " . gethostname();
+echo "<br><br>php versja:" . phpversion();
+echo "<br>";
 ?>
